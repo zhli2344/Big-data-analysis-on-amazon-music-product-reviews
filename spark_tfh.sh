c2c5cd0911d120582283075a0dceaa26042bb093
Zhuoyang Li <48282284+zhli2344@users.noreply.github.com>
2019-07-22 22:56:28 +0800

    Add files via upload

---
+++ spark_tfh.sh
@@ -0,0 +1,6 @@
+sudo yum -y install git
+sudo pip-3.6 install --quiet tensorflow-hub 
+sudo pip-3.6 install --quiet numpy
+sudo python3 -m nltk.downloader -d /usr/share/nltk_data all
+sudo pip-3.6 install --quiet pandas
+sudo pip-3.6 install --quiet matplotlib
